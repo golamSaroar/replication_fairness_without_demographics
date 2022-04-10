af094ed10df63e74f5214272dfd5d1c06c0a88dc
Sk Golam Saroar <emailsaroar@gmail.com>
2022-04-10 00:34:15 -0400

    add processed data for compas

--- data_preprocessing/data_utils.py
+++ data_preprocessing/data_utils.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import os
+import json
 
 
+# convert object columns to category columns
+# https://stackoverflow.com/a/39906514/4082505
 def convert_object_to_category(df):
     return pd.concat([
         df.select_dtypes([], ['object']),
@@ -9,18 +12,50 @@ def convert_object_to_category(df):
     ], axis=1).reindex(df.columns, axis=1)
 
 
-def process_dict(df, dict, column):
-    df[column] = df.apply(lambda x: dict[x[column]] if x[column] in dict.keys() else 'Other',
+def process(df, dictionary, column):
+    df[column] = df.apply(lambda x: dictionary[x[column]] if x[column] in dictionary.keys() else 'Other',
                           axis=1).astype('category')
 
 
 def process_protected_columns(df, race_dict=None, sex_dict=None):
     if race_dict is not None:
-        process_dict(df, race_dict, 'race')
+        process(df, race_dict, 'race')
     if sex_dict is not None:
-        process_dict(df, sex_dict, 'sex')
+        process(df, sex_dict, 'sex')
 
 
-def write_to_output_file(train_df, test_df, base_dir):
-    train_df.to_csv(os.path.join(base_dir, 'train.csv'), index=False, header=True)
-    test_df.to_csv(os.path.join(base_dir, 'test.csv'), index=False, header=True)
+def write_to_output_file(base_dir, filename, content):
+    output_file_path = os.path.join(base_dir, filename)
+
+    with open(output_file_path, mode="w") as output_file:
+        output_file.write(json.dumps(content))
+        output_file.close()
+
+
+def construct_vocabulary(df, base_dir):
+    categorical_columns = df.select_dtypes(include='category').columns
+    vocab_dict = {}
+
+    for column in categorical_columns:
+        vocab_dict[column] = list(set(df[column].cat.categories))
+    write_to_output_file(base_dir, "vocabulary.json", vocab_dict)
+
+
+def construct_mean_std(df, base_dir):
+    dictionary = df.describe().to_dict()
+    mean_std_dict = {}
+
+    for key, value in dictionary.items():
+        mean_std_dict[key] = [value['mean'], value['std']]
+    write_to_output_file(base_dir, "mean_and_std.json", mean_std_dict)
+
+
+def save_processed_data(base_dir, train_df, test_df):
+    train_data = os.path.join(base_dir, 'train.csv')
+    test_data = os.path.join(base_dir, 'test.csv')
+
+    train_df.to_csv(train_data, index=False, header=True)
+    test_df.to_csv(test_data, index=False, header=True)
+
+    construct_vocabulary(train_df, base_dir)
+    construct_mean_std(train_df, base_dir)
--- data_preprocessing/preprocess_compus_data.py
+++ data_preprocessing/preprocess_compus_data.py
@@ -2,27 +2,27 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from data_utils import convert_object_to_category, process_protected_columns, write_to_output_file
+from data_utils import convert_object_to_category, process_protected_columns, save_processed_data
 
 base_dir = "data/compas"
 file_path = os.path.join(base_dir, "compas-scores-two-years.csv")
 
 columns = ["juv_fel_count", "juv_misd_count", "juv_other_count", "priors_count", "age", "c_charge_degree",
            "c_charge_desc", "age_cat", "sex", "race", "is_recid"]
-target_variable = "is_recid"
-target_value = "Yes"
 
-df = pd.read_csv(file_path, sep=',', names=columns, skipinitialspace=True)
-df = convert_object_to_category(df)
-# drop duplicates
-df = df[["id"] + columns].drop_duplicates()
-df = df[columns]
-
-# Convert race to Black, White and Other
 race_dict = {
     "African-American": "Black",
     "Caucasian": "White"
 }
+
+target_variable = "is_recid"
+target_value = "Yes"
+
+df = pd.read_csv(file_path, sep=',')
+df = convert_object_to_category(df)
+# drop duplicates
+df = df[['id']+columns].drop_duplicates()
+df = df[columns]
 
 process_protected_columns(df, race_dict=race_dict)
 
@@ -31,4 +31,4 @@ df = df.dropna()
 
 train_df, test_df = train_test_split(df, test_size=0.30, random_state=42)
 
-write_to_output_file(train_df, test_df, base_dir)
+save_processed_data(base_dir, train_df, test_df)
